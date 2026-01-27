900c8160d90d5d16e60d4a07d460ec2006af8d36
NGLSG <927039685@qq.com>
2026-01-27 14:11:40 +0800

    Fix EffectText and TextEffectConfig export conflicts in vn_rendering

--- lib/src/runtime/rendering/vn_rendering.dart
+++ lib/src/runtime/rendering/vn_rendering.dart
@@ -11,8 +11,8 @@ export 'background_layer.dart' hide BackgroundTransition;
 // Character rendering
 export 'character_layer.dart';
 
-// Screen and text effects
-export 'effects_layer.dart';
+// Screen and text effects (hide duplicates defined in effects/text/)
+export 'effects_layer.dart' hide EffectText, TextEffectConfig;
 
 // Audio management
 export 'audio_manager.dart';
